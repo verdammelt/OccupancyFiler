c693779732c72b0fa5d200c75d2cf062c6f63c32
Mark Simpson <verdammelt@gmail.com>
2012-11-21 19:59:35 -0500

    adding logging to the end of the process

--- src/main/groovy/OccupancyFiler/Filer.groovy
+++ src/main/groovy/OccupancyFiler/Filer.groovy
@@ -12,17 +12,18 @@ class Filer {
     }
 
     void performFiling() {
-        def logProcessing = {File file -> log("processing ${file?.absolutePath}"); file }
+        def logStart = {File file -> log("processing ${file?.absolutePath}"); file }
         def logTrimmed = {File file -> log("trimmed..."); file }
         def logRenamed = {File file -> log("renamed to ${file?.name}"); file }
         def logMoved = { File file -> log("moved to ${file?.absolutePath}"); file }
+        def logDone = { File file -> log("processing completed")}
 
         def trim = arguments.trimmer.&trimTopLines >> logTrimmed
         def rename = arguments.renamer.&rename >> logRenamed
         def move = arguments.mover.&move >> logMoved
         def incrSeqNum = { File file -> arguments.sequenceNumber.commit(); file }
 
-        def process = logProcessing >> trim >> rename >> move >> incrSeqNum
+        def process = logStart >> trim >> rename >> move >> incrSeqNum >> logDone
 
         arguments.files.each process
     }
